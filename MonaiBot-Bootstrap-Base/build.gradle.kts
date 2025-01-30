5616a90c54d8d339f9fa1a61c78820fbbbf799c6
MrHua269 <wangxyper@163.com>
2025-01-30 08:20:05 +0800

    ShadowJar

--- MonaiBot-Bootstrap-Base/build.gradle.kts
+++ MonaiBot-Bootstrap-Base/build.gradle.kts
@@ -1,5 +1,8 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
     id("java")
+    id("io.github.goooler.shadow") version "8.1.2"
 }
 
 dependencies {
@@ -7,4 +10,14 @@ dependencies {
     implementation(project(":MonaiBot-Functions"))
     implementation("org.jetbrains:annotations:24.0.1")
     implementation("org.apache.logging.log4j:log4j-api:2.22.0")
+}
+
+tasks.withType(ShadowJar::class) {
+    manifest {
+        attributes("Main-Class" to "i.earthme.monaibot.boot.Main")
+    }
+    exclude("META-INF/*.RSA", "META-INF/*.SF", "META-INF/*.DSA")
+    exclude("META-INF/NOTICE", "META-INF/NOTICE.txt")
+    exclude("META-INF/LICENSE", "META-INF/LICENSE.txt")
+    exclude("META-INF/DEPENDENCIES")
 }
